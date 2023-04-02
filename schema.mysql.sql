6c716b70a993550376caac7ff4566772e93fde99
Aaron Parecki <aaron@parecki.com>
2023-04-02 14:49:14 -0700

    add separate mysql schema file
    also cleaned up the postgres schema to remove the duplicate table and unnecessary sql

---
+++ schema.mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE answers (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    date DATETIME,
+    username VARCHAR(255),
+    prompt TEXT,
+    question TEXT,
+    prompt_id VARCHAR(255),
+    status VARCHAR(255)
+);
+
